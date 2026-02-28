85d13c5a102301845367711d463c8ac47db03284
Airat Zaripov <lucky_hunter@list.ru>
2026-02-28 16:43:46 +0100

    feature/GPMS-1 - init service

---
+++ src/main/java/com/gameplatform/main_service/GamePlatformMainServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gameplatform.main_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamePlatformMainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamePlatformMainServiceApplication.class, args);
+	}
+
+}
