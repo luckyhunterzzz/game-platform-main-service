b03d35271799f3df91c8d48041a6241ed5d67d83
Airat Zaripov <lucky_hunter@list.ru>
2026-05-22 21:27:59 +0400

    feature/GPMS-44 - hero coach

---
+++ src/main/java/com/gameplatform/mainservice/hero/dto/response/HeroCoachHeroResponse.java
@@ -0,0 +1,24 @@
+package com.gameplatform.mainservice.hero.dto.response;
+
+import java.time.LocalDate;
+
+public record HeroCoachHeroResponse(
+        Long id,
+        String slug,
+        String name,
+        String imageUrl,
+        String previewUrl,
+        String elementName,
+        String rarityName,
+        Integer rarityStars,
+        String heroClassName,
+        String manaSpeedName,
+        String familyName,
+        String alphaTalentName,
+        Integer baseAttack,
+        Integer baseArmor,
+        Integer baseHp,
+        LocalDate releaseDate,
+        LocalDate heroCoachDate
+) {
+}
